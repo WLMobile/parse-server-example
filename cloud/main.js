366f6cdd8068809ec86a1af7f0b439723a8b5721
hanih <dev00@mobidev.co>
2016-10-02 22:09:47 +0300

    Added sync function

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,17 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hello');
 });
+
+Parse.Cloud.define('sync', function(req, res){
+	var Class = Parse.Object.extend(req.class);
+	var class = new Class();
+	class.set(req.data);
+	class.save(null, {
+		success: function(class){
+			response.success(class);
+		},
+		error: function(error){
+			response.error(error);
+		}
+	})
+});

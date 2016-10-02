bc6002df3ae9b199092480b64dfcd8a4b3f097e5
hanih <dev00@mobidev.co>
2016-10-02 23:04:21 +0300

    8

--- cloud/main.js
+++ cloud/main.js
@@ -6,8 +6,8 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define('syncData', function(req, response){
 	var Class = Parse.Object.extend(req.params.parseClass);
 	var parseClass = new Class();
-	parseClass.set(req.params.data);
-	parseClass.save(null, {
+	//parseClass.set(req.params.data);
+	parseClass.save(req.params.data, {
 		success: function(parseClass){
 			response.success(parseClass);
 		},

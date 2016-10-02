ea6dc0f3ce86c120d4a9518879b542e4c007f8f9
hanih <dev00@mobidev.co>
2016-10-03 02:03:34 +0300

    Update ACL

--- cloud/main.js
+++ cloud/main.js
@@ -7,7 +7,12 @@ Parse.Cloud.define('syncData', function(req, response){
 	var CC = Parse.Object.extend("Batches");
 	//Parse.Cloud.useMasterKey();
 	var parseClass = new CC();
-	parseClass.setACL(new Parse.ACL(Parse.User.current()));
+	var newACL = new Parse.ACL();
+    newACL.setPublicReadAccess(false);
+    newACL.setPublicWriteAccess(false);
+    newACL.setReadAccess(req.params.data.userId,true);
+    newACL.setWriteAccess(req.params.data.userId,true);
+	parseClass.setACL(newACL);
 	parseClass.save(req.params.data, {
 		success: function(parseClass){
 			response.success(parseClass);

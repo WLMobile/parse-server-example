5df9100886ba5e4399f67bb4898f49a2f5fb78f8
hanih <dev00@mobidev.co>
2016-10-02 23:54:00 +0300

    16

--- cloud/main.js
+++ cloud/main.js
@@ -4,30 +4,16 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define('syncData', function(req, response){
-	var PersonClass = Parse.Object.extend("Person");
-var person = new PersonClass();
-
-person.set("age",25);
-
-person.save(null,{
-  success:function(person) { 
-    response.success(person);
-  },
-  error:function(object, res) {
-    response.error(res);
-  }
-});
-	//var Class = Parse.Object.extend(req.params.parseClass);
-	//var parseClass = new Class();
-	//parseClass.set("batchName":"test");
-	// parseClass.save(null, {
-	// 	success: function(parseClass){
-	// 		response.success(parseClass);
-	// 	},
-	// 	error: function(error){
-	// 		response.error(error);
-	// 	}
-	// });
-
-	//response.success('succes...');
+	
+	var Class = Parse.Object.extend(req.params.parseClass);
+	var parseClass = new Class();
+	parseClass.set("batchName":"test");
+	parseClass.save(null, {
+		success: function(parseClass){
+			response.success(parseClass);
+		},
+		error: function(error){
+			response.error(error);
+		}
+	});
 });

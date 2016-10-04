ae354947877822ddf8c25457ba2b26f3812ad26e
hanih <dev00@mobidev.co>
2016-10-04 22:41:33 +0300

    Added listarray function

--- cloud/main.js
+++ cloud/main.js
@@ -8,7 +8,7 @@ Parse.Cloud.define('updateData', updateData);
 Parse.Cloud.define('syncData', syncData);
 
 Parse.Cloud.define('syncArray', syncArray);
-//Parse.Cloud.define('listArray', listArray);
+Parse.Cloud.define('listArray', listArray);
 
 function updateData(req, response) {
 	//var CC = Parse.Object.extend(req.params.parseClass);
@@ -57,27 +57,27 @@ function updateData(req, response) {
 
 }
 
-// function listArray(req, response){
-// 	response.success("OK");
-// 	// listObjects(req.params).then(function(results){
-// 	// 	response.success(results);
-// 	// });
+function listArray(req, response){
+	response.success("OK");
+	listObjects(req.params).then(function(results){
+		response.success(results);
+	});
 	
-// }
+}
 
-// function findObject(obj){
-// 	var query = new Parse.Query(obj.parseClass);
-// 	query.equalTo("oid", obj.oid);
-// 	return query.find();
-// }
+function findObject(obj){
+	var query = new Parse.Query(obj.parseClass);
+	query.equalTo("oid", obj.oid);
+	return query.find();
+}
 
-// function listObjects(array){
-// 	var promisesArray = [];
-// 	for(var i =0; i< array.length; i++){
-// 		promisesArray.push(findObject(array[i]));
-// 	}
-// 	return Parse.Promise.when(promisesArray);
-// }
+function listObjects(array){
+	var promisesArray = [];
+	for(var i =0; i< array.length; i++){
+		promisesArray.push(findObject(array[i]));
+	}
+	return Parse.Promise.when(promisesArray);
+}
 
 function syncArray(req, response) {
 	var modelArray = [];

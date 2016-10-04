a5934bfd8cbfed1d2600acb336d7d5f4b704c2d3
hanih <dev00@mobidev.co>
2016-10-04 19:06:42 +0300

    Insert in case of new row

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,5 @@
-
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hello');
+	res.success('Hello');
 });
 
 Parse.Cloud.define('updateData', updateData);
@@ -9,48 +8,64 @@ Parse.Cloud.define('syncData', syncData);
 
 Parse.Cloud.define('SyncArray', syncArray);
 
-
-function updateData(req, response){
+function updateData(req, response) {
 	//var CC = Parse.Object.extend(req.params.parseClass);
 	var query = new Parse.Query(req.params.parseClass);
 	console.log('OK');
 	query.equalTo("oid", req.params.data.oid);
 	query.first({
-		sessionToken: req.user.getSessionToken(),
-		success: function(obj){
-			if(obj){
+		sessionToken : req.user.getSessionToken(),
+		success : function(obj) {
+			if (obj) {
 				obj.set(req.params.data);
 				obj.save(null, {
-					sessionToken: req.user.getSessionToken(),
-					success: function(parseClass){
+					sessionToken : req.user.getSessionToken(),
+					success : function(parseClass) {
 						response.success(parseClass);
 					},
-					error: function(data,error){
+					error : function(data, error) {
 						response.success("Object not saved");
 					}
 				});
 			} else {
-				response.success('No object found');
+				var CC = Parse.Object.extend(req.params.parseClass);
+				var parseClass = new CC();
+				var newACL = new Parse.ACL();
+				newACL.setPublicReadAccess(false);
+				newACL.setPublicWriteAccess(false);
+				newACL.setReadAccess(req.params.data.userId, true);
+				newACL.setWriteAccess(req.params.data.userId, true);
+				parseClass.setACL(newACL);
+				parseClass.set(req.params.data);
+				parseClass.save(null, {
+					success : function(parseClass) {
+						response.success('Object not existing. Creating a new one');
+					},
+					error : function(data, error) {
+						response.error(error);
+					}
+				});
+
 			}
-			
+
 		},
-		error: function(obj, error){
+		error : function(obj, error) {
 			response.success("No result found");
 		}
 	});
 
 }
 
-function syncArray(req, response){
+function syncArray(req, response) {
 	var modelArray = [];
 	var newACL = new Parse.ACL();
-    newACL.setPublicReadAccess(false);
-    newACL.setPublicWriteAccess(false);
-    newACL.setReadAccess(req.params.userId,true);
-    newACL.setWriteAccess(req.params.userId,true);
+	newACL.setPublicReadAccess(false);
+	newACL.setPublicWriteAccess(false);
+	newACL.setReadAccess(req.params.userId, true);
+	newACL.setWriteAccess(req.params.userId, true);
 	var dataArray = req.params.data;
 
-	for(var i=0; i< dataArray.length; i++){
+	for (var i = 0; i < dataArray.length; i++) {
 		var CC = Parse.Object.extend(dataArray[i].parseClass);
 		var parseClass = new CC();
 		parseClass.set(dataArray[i].data);
@@ -58,34 +73,35 @@ function syncArray(req, response){
 		modelArray[i] = parseClass;
 	}
 
-	Parse.Object.saveAll(modelArray,{
-    success: function(list) {
-      // All the objects were saved.
-      response.success("ok " );  //saveAll is now finished and we can properly exit with confidence :-)
-    },
-    error: function(error) {
-      // An error occurred while saving one of the objects.
-      response.error("failure on saving list ");
-    },
-  });
+	Parse.Object.saveAll(modelArray, {
+		success : function(list) {
+			// All the objects were saved.
+			response.success("ok ");
+			//saveAll is now finished and we can properly exit with confidence :-)
+		},
+		error : function(error) {
+			// An error occurred while saving one of the objects.
+			response.error("failure on saving list ");
+		},
+	});
 }
 
-function syncData(req, response){
+function syncData(req, response) {
 	var CC = Parse.Object.extend(req.params.parseClass);
 	//Parse.Cloud.useMasterKey();
 	var parseClass = new CC();
 	var newACL = new Parse.ACL();
-    newACL.setPublicReadAccess(false);
-    newACL.setPublicWriteAccess(false);
-    newACL.setReadAccess(req.params.data.userId,true);
-    newACL.setWriteAccess(req.params.data.userId,true);
+	newACL.setPublicReadAccess(false);
+	newACL.setPublicWriteAccess(false);
+	newACL.setReadAccess(req.params.data.userId, true);
+	newACL.setWriteAccess(req.params.data.userId, true);
 	parseClass.setACL(newACL);
 	parseClass.set(req.params.data);
 	parseClass.save(null, {
-		success: function(parseClass){
+		success : function(parseClass) {
 			response.success(parseClass);
 		},
-		error: function(data,error){
+		error : function(data, error) {
 			response.error(error);
 		}
 	});

c3c6fab80bd3e0a005825a84efd5d05bc2cabd27
hanih <dev00@mobidev.co>
2016-10-04 05:14:28 +0300

    add session token

--- cloud/main.js
+++ cloud/main.js
@@ -9,7 +9,7 @@ Parse.Cloud.define('updateData', function(req, response){
 	console.log('OK');
 	//query.equalTo("oid", req.params.data.oid);
 	query.find({
-		useMasterKey: true,
+		sessionToken: req.user.getSessionToken(),
 		success: function(obj){
 			//obj.set(req.params.data);
 			response.success(obj);

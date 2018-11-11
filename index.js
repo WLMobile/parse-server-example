86124b3ceef94eaa7c2a2f1f5cd63f2c8cab9933
hanih <hamadeh.hani@gmail.com>
2018-11-11 19:53:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,15 +36,13 @@ var api = new ParseServer({
         },
       passwordResetEmail: {
           subject: 'Reset your password',
-          pathPlainText: resolve(__dirname, 'path/to/templates/password_reset_email.txt'),
-          pathHtml: resolve(__dirname, 'path/to/templates/password_reset_email.html'),
+          pathPlainText: path.join(__dirname, '/public/verification_email.txt'),
           callback: (user) => { return { firstName: user.get('firstName') }}
           // Now you can use {{firstName}} in your templates
         },
       customEmailAlert: {
           subject: 'Urgent notification!',
-          pathPlainText: resolve(__dirname, 'path/to/templates/custom_alert.txt'),
-          pathHtml: resolve(__dirname, 'path/to/templates/custom_alert.html'),
+          pathPlainText: path.join(__dirname, '/public/verification_email.txt')
         }
     }
   },
